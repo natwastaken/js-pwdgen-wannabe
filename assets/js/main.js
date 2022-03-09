55095fdc2a890c93907bfae1636895add01589e8
natwastaken <francesconatoli01@gmail.com>
2022-03-09 09:51:14 +0100

    password gen

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+
+let nomeUtente = prompt ("Inserisci il tuo nome");
+console.log(nomeUtente);
+
+let cognomeUtente = prompt ("inserisci il tuo cognome");
+console.log(cognomeUtente);
+
+let coloreUtente = prompt ("Inserisci il tuo colore preferito");
+console.log (coloreUtente);
+
+
+let passwordUtente = nomeUtente + cognomeUtente + coloreUtente + 21;
